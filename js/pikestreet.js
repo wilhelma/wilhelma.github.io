77fad2206267f6d3efbcc47b08ff3dd7f01a12dd
Andreas Wilhelm <wilhelma@in.tum.de>
2018-12-17 15:31:54 +0100

    first commit

---
+++ js/pikestreet.js
@@ -0,0 +1,49 @@
+Reveal.initialize({
+    progress: true,
+    history: true,
+    center: true,
+    controls: false,
+    fragments: true,
+
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+    transitionSpeed: 'default', // default/fast/slow
+
+    // Optional reveal.js plugins
+    dependencies: [{
+        src: 'lib/js/classList.js',
+        condition: function () {
+            return !document.body.classList;
+        }
+    }, {
+        src: 'plugin/markdown/marked.js',
+        condition: function () {
+            return !!document.querySelector('[data-markdown]');
+        }
+    }, {
+        src: 'plugin/markdown/markdown.js',
+        condition: function () {
+            return !!document.querySelector('[data-markdown]');
+        }
+    }, {
+        src: 'plugin/highlight/highlight.js',
+        async: true,
+        condition: function () {
+            return !!document.querySelector('pre code');
+        },
+        callback: function () {
+            hljs.initHighlightingOnLoad();
+        }
+    }, {
+        src: 'plugin/zoom-js/zoom.js',
+        async: true
+    }, {
+        src: 'plugin/notes/notes.js',
+        async: true
+    }]
+});
+
+Reveal.configure({ slideNumber: 'c' });
+
+$(document).ready(function () {
+    $('video').attr('loop','loop');
+})
